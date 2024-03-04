b196ebd111421c1f000e0064ec8124d4c9efc1e8
Abraham V. R <sneak_89@hotmail.com>
2024-03-04 09:33:51 -0600

    Agregar archivos de proyecto.

---
+++ Herencia/Gato.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    //Sintaxis Herencia
+    // Tipo de acceso class _NombreClaseDerivada_ : _NombreClaseBase_
+    internal class Gato : Mamifero
+    {
+        //Atributos
+        private int nBigotes { get; set; }
+
+        //Constructor
+        /* Cuando se hereda un constructor con parametros
+         * se tienen que colocar los parametros en el constructor
+         * de la clase derivada (o subclase), de lo contrario
+         * se mantendra un errro
+         */
+        public Gato(string n, double p, int b) : base(n,p)
+        {
+            this.nBigotes = b;
+        }
+
+        //Metodos
+        public void mostrarGato()
+        {
+            string nom = "";
+            nom = getNombre(); 
+            Console.WriteLine("El nombre es: " + nom);
+            Console.WriteLine("Su peso en Kg es de: " + peso); 
+            Console.WriteLine("Tiene " + nBigotes + " bigotes completos");
+        }
+    }
+}
---
+++ Herencia/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Variables 
+using Herencia;
+
+string nombre;
+double peso;
+int nBigotes; 
+
+//Ingresar y mostrar clase gato
+Console.WriteLine("------Clase gato------");
+Console.Write("Ingresa el nombre del gato: ");
+nombre = Console.ReadLine();
+
+Console.Write("Ingresa el peso: "); 
+peso = double.Parse(Console.ReadLine());
+
+Console.Write("Ingresa el numero de bigotes: ");
+nBigotes = int.Parse(Console.ReadLine());
+
+//Instancia de los objetos
+//Gato 
+Gato gato1 = new Gato(nombre, peso, nBigotes);
+
+Console.WriteLine(); 
+gato1.mostrarGato();
+
+//Cambiando el nombre al gato
+gato1.setNombre("Lucifer");
+nombre = gato1.getNombre();
+Console.WriteLine("\n---Le cambie el nombre al gato----\n");
+gato1.mostrarGato(); 
